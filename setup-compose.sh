822b2e77b4a15a61bf8344ab2eee84e9542a9398
Ramiz Dundar <ramizdundar@gmail.com>
2024-02-10 17:04:15 -0500

    Add build script to docker compose setup

--- setup-compose.sh
+++ setup-compose.sh
@@ -17,5 +17,7 @@ PASH_TOP=$(realpath $DISH_TOP/pash)
 $PASH_TOP/scripts/setup-pash.sh
 cp $DISH_TOP/docker-hadoop/dish-config.json $PASH_TOP/cluster.json
 
+$DISH_TOP/runtime/scripts/build.sh
+
 # https://docs.docker.com/compose/migrate
 docker-compose up -d || docker compose up -d
