4466bdd736f8da720299c2d14140e9fda300e84b
Ramiz Dundar <ramizdundar@gmail.com>
2024-02-18 00:26:48 -0500

    Update docker builld process to support mounting

--- setup-compose.sh
+++ setup-compose.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-if [ "$1" == '--eval' ]; then
-    export RELEASE="eval"
-else
-    export RELEASE="latest"
-fi
+for arg in "$@"
+do
+    if [ "$arg" == '--eval' ]; then
+        export RELEASE="eval"
+    elif [ "$arg" == '--no-cache' ]; then
+        export NO_CACHE=1
+    fi
+done
 
 ## TODO: This should not build the images by default, but should just download them
 make build
 
-docker-compose up -d
+DISH_TOP=$(realpath $(dirname "$0")/..)
+PASH_TOP=$(realpath $DISH_TOP/pash)
+
+# install libtool if this fails, alternatively you can install distro deps
+# sudo apt install libtool
+echo "n" | $PASH_TOP/scripts/setup-pash.sh
+cp $DISH_TOP/docker-hadoop/dish-config.json $PASH_TOP/cluster.json
+
+$DISH_TOP/runtime/scripts/build.sh
+
+# https://docs.docker.com/compose/migrate
+docker-compose up -d || docker compose up -d
