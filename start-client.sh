905dfcfb353816a69390f16764a3f0e3f9ff2c2c
Ramiz Dundar <ramizdundar@gmail.com>
2024-02-21 11:20:20 -0500

    Update start-client to include DISH_TOP and compose updates

--- start-client.sh
+++ start-client.sh
@@ -5,7 +5,15 @@ else
     export RELEASE="latest"
 fi
 
+# Check if DISH_TOP is set
+if [ -z "$DISH_TOP" ]
+then
+    # If not set, assign a default path
+    export DISH_TOP=$(realpath $(dirname "$0")/../..)
+fi
+
 echo "Generating config"
 ./gen_config.sh
 
-docker-compose -f docker-compose-client.yml up -d
+# https://docs.docker.com/compose/migrate
+docker compose -f docker-compose-client.yml up -d || docker-compose -f docker-compose-client.yml up -d
