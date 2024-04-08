816b33e9ced844f82a2d6a3aee09397e5f38028c
Zhicheng Huang <skyhuang@umich.edu>
2024-04-08 13:11:39 -0400

    Start a worker server on node_manager to handle re-exec

--- nodemanager/run.sh
+++ nodemanager/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-"$DISH_TOP/runtime/bin/discovery_server" > /discovery.log &
+# The current design requires 1) discovery_server running on the worker_manager node and
+#                             2) worker.py server running on the client node
+# Because right now worker_manager node and client node are the same node, I simply call worker.sh
+#               which spawns both the discovery_server and worker.py
+bash $PASH_TOP/compiler/dspash/worker.sh &> worker.log &
 
 $HADOOP_HOME/bin/yarn --config $HADOOP_CONF_DIR nodemanager
