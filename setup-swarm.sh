acf8cf7c31b895f80f9290eedfb89825b19811b7
Zhicheng Huang <skyhuang@umich.edu>
2024-06-27 01:26:40 -0400

    Revert cloudlab setup scripts

--- setup-swarm.sh
+++ setup-swarm.sh
@@ -1,77 +1,135 @@
-#!/bin/bash
+version: '3.5'
 
-# Determine the release tag
-if [ "$1" == '--eval' ]; then
-    export RELEASE="eval"
-else
-    export RELEASE="latest"
-fi
+services:
+  namenode:
+    image: localhost:5000/hadoop-namenode:$RELEASE
+    networks:
+      - hbase
+    volumes:
+      - namenode:/hadoop/dfs/name
+      - ${PWD}/dspash-config.json:${DISH_TOP}/pash/cluster.json
+    environment:
+      - CLUSTER_NAME=test
+    env_file:
+      - ./hadoop.env
+    deploy:
+      mode: replicated
+      endpoint_mode: dnsrr
+      replicas: 1
+      restart_policy:
+        condition: on-failure
+      placement:
+        constraints:
+          - node.role == manager
+      labels:
+        traefik.docker.network: hbase
+        traefik.port: 9870
 
-# Set Docker client timeout
-export DOCKER_CLIENT_TIMEOUT=300
-export COMPOSE_HTTP_TIMEOUT=300
+  datanode:
+    image: localhost:5000/hadoop-datanode:$RELEASE
+    hostname: "{{.Node.Hostname}}-datanode"
+    networks:
+      - hbase
+    volumes:
+      - datanode:/hadoop/dfs/data
+    env_file:
+      - ./hadoop.env
+    environment:
+      SERVICE_PRECONDITION: "namenode:9870"
+    ulimits:
+      nofile:
+        soft: 1000000
+        hard: 1000000
+    deploy:
+      mode: global
+      endpoint_mode: dnsrr
+      placement:
+        constraints:
+          - node.role != manager
+      restart_policy:
+        condition: on-failure
+      labels:
+        traefik.docker.network: hbase
+        traefik.port: 9864
+    cap_add:
+      - SYS_PTRACE
+    security_opt:
+      - seccomp:unconfined
 
-# Function to check if a service is running
-function service_running() {
-    local service_name="$1"
-    docker service ls --filter "name=${service_name}" --format "{{.Name}}"
-}
+  resourcemanager:
+    image: localhost:5000/hadoop-resourcemanager:$RELEASE
+    networks:
+      - hbase
+    environment:
+      SERVICE_PRECONDITION: "namenode:9870 datanode:9864"
+    env_file:
+      - ./hadoop.env
+    deploy:
+      mode: replicated
+      endpoint_mode: dnsrr
+      replicas: 1
+      restart_policy:
+        condition: on-failure
+      placement:
+        constraints:
+          - node.role == manager
+      labels:
+        traefik.docker.network: hbase
+        traefik.port: 8088
+    healthcheck:
+      disable: true
 
-# Function to check if a network is created
-function network_exists() {
-    local network_name="$1"
-    docker network ls --filter "name=${network_name}" --format "{{.Name}}"
-}
+  nodemanager:
+    image: localhost:5000/hadoop-nodemanager:$RELEASE
+    networks:
+      - hbase
+    environment:
+      SERVICE_PRECONDITION: "namenode:9870 datanode:9864 resourcemanager:8088"
+    env_file:
+      - ./hadoop.env
+    ulimits:
+      nofile:
+        soft: 1000000
+        hard: 1000000
+    deploy:
+      mode: global
+      endpoint_mode: dnsrr
+      placement:
+        constraints:
+          - node.role != manager
+      restart_policy:
+        condition: on-failure
+      labels:
+        traefik.docker.network: hbase
+        traefik.port: 8042
 
-# Build the Docker images
-echo "Building the Docker images"
-make build
+  historyserver:
+    image: localhost:5000/hadoop-historyserver:$RELEASE
+    networks:
+      - hbase
+    volumes:
+      - hadoop_historyserver:/hadoop/yarn/timeline
+    environment:
+      SERVICE_PRECONDITION: "namenode:9870 datanode:9864 resourcemanager:8088"
+    env_file:
+      - ./hadoop.env
+    deploy:
+      mode: replicated
+      replicas: 1
+      placement:
+        constraints:
+          - node.role == manager
+      labels:
+        traefik.docker.network: hbase
+        traefik.port: 8188
 
-# Setup the HBase network
-echo "Setting up the HBase network"
-if [ -z "$(network_exists hbase)" ]; then
-    docker network create -d overlay --attachable hbase
-else
-    echo "Network 'hbase' already exists"
-fi
+volumes:
+  datanode:
+  namenode:
+  hadoop_historyserver:
 
-# Setup a local image registry
-echo "Setting up a local image registry"
-if [ -z "$(service_running registry)" ]; then
-    docker service create --name registry --publish published=5000,target=5000 registry:2
-else
-    echo "Registry service is already running"
-fi
 
-# Wait for the registry to be fully up
-sleep 10
-
-# Push images to the local registry
-images=(
-    "hadoop-historyserver"
-    "hadoop-nodemanager"
-    "hadoop-resourcemanager"
-    "hadoop-datanode"
-    "hadoop-namenode"
-    "hadoop-pash-base"
-)
-
-for image in "${images[@]}"; do
-    echo "Pushing $image"
-    docker image tag "$image:$RELEASE" "127.0.0.1:5000/$image:$RELEASE"
-    docker image push "127.0.0.1:5000/$image:$RELEASE"
-
-    if [ $? -ne 0 ]; then
-        echo "Error pushing $image:$RELEASE to 127.0.0.1:5000/$image:$RELEASE"
-        exit 1
-    fi
-done
-
-# Deploy the Swarm
-echo "Deploying the Swarm"
-docker stack deploy -c docker-compose-v3.yml hadoop
-
-# Generate the configuration
-./gen_config.sh
-
-echo "Deployment completed successfully."
+networks:
+  hbase:
+    name: hbase
+    external: true
